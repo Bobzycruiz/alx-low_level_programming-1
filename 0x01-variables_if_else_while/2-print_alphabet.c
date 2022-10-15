349e22ce3948932e7d346c5a0201307ca7bf5de1
Ikar700 <Cornex111@gmail.com>
2022-10-14 21:20:41 -0700

    Task 2

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,1 +1,21 @@
-§
+#include <stdio.h>
+
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char 1;
+
+	l = 'a';
+	while (l <= 'z')
+	{
+		putchar(1);
+		l++;
+	}
+	putchar('\n');
+	return (0);
+}
