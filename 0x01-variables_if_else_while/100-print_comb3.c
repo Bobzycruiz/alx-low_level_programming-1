6a3e15e7865f600e48770aa9aeec3e2dd53ea2a0
Olubadejo Cornelius Fasae <77524855+Ikar700@users.noreply.github.com>
2022-10-15 07:24:45 +0100

    Task 10

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,13 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/* betty style doc for function main oes there */
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+  
